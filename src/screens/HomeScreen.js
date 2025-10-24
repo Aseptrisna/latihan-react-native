ac8d9011030d07d1ff9af93cda3ca2155bc050de
A. Trisna Setiawan <aseptrisna1995@gmail.com>
2025-10-24 14:14:32 +0700

    .

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { 
-  View, 
-  Text, 
-  StyleSheet, 
-  TouchableOpacity, 
-  SafeAreaView, 
-  StatusBar 
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar
 } from 'react-native';
 
 // Terima 'navigation' dari props
@@ -13,7 +13,7 @@ const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#4A90E2" />
-      
+
       {/* === HEADER === */}
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Beranda</Text>
@@ -24,22 +24,22 @@ const HomeScreen = ({ navigation }) => {
       <View style={styles.menuContainer}>
 
         {/* --- Card 1: Data Mahasiswa --- */}
-        <TouchableOpacity 
-          style={styles.card} 
+        <TouchableOpacity
+          style={styles.card}
           onPress={() => navigation.navigate('StudentList')}>
           <Text style={styles.cardText}>Data Mahasiswa</Text>
         </TouchableOpacity>
 
         {/* --- Card 2: Buka Profil --- */}
-        <TouchableOpacity 
-          style={styles.card} 
+        <TouchableOpacity
+          style={styles.card}
           onPress={() => navigation.navigate('Profile')}>
           <Text style={styles.cardText}>Profile Saya</Text>
         </TouchableOpacity>
 
         {/* --- Card 3: Logout --- */}
-        <TouchableOpacity 
-          style={[styles.card, styles.cardLogout]} 
+        <TouchableOpacity
+          style={[styles.card, styles.cardLogout]}
           onPress={() => navigation.replace('Login')}>
           <Text style={[styles.cardText, styles.logoutText]}>Logout</Text>
         </TouchableOpacity>
@@ -111,7 +111,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF0F0',
     borderColor: '#E74C3C',
     borderWidth: 1,
-    elevation: 4, 
+    elevation: 4,
   },
   logoutText: {
     color: '#E74C3C',
